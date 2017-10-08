4dc9e8326bd16508cfb722c4c2eb0d1f5837ef64
Vlad Fratila <vlad.fratila@gmail.com>
2017-10-08 13:52:56 +0300

    fix js errors

--- themes/wefight/static/js/nav.js
+++
@@ -1,7 +0,0 @@
-($(document).ready(function(){
-    $('.navbar-burger').click(function () {
-        var id = $(this).attr('data-target');
-        $('#'+id).toggleClass('is-active');
-        $(this).toggleClass('is-active');
-    });
-}))(jQuery);
